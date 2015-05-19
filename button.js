e93d3423f823157d948e1d36dd474fcc4acf2caf
Mark Gorenstein <markagorenstein@gmail.com>
2015-05-19 13:09:30 +0100

    new button attempt

--- button.js
+++ button.js
@@ -1,10 +1,13 @@
 ---
 ---
 
-document.getElementsByClassName("button").onclick = function () {
+var button = document.getElementById('button');
+button.onclick = goToNewSentence;
+
+function goToNewSentence() {
     location.href = "{{ site.baseurl }}/sentences/" + getRandomInt();
-};
+}
 
 function getRandomInt() {
-    return Math.floor(Math.random() * {{ site.collections.sentences | size }} + 1);
+    return Math.floor(Math.random() * {{ site.collections.sentences.docs | size }} + 1);
 }; 
