06f8a090efad71a0100a5795f6b58b1a2e1b4675
Mark Gorenstein <markagorenstein@gmail.com>
2015-05-19 13:59:55 +0100

    automation

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/workspace/monkeystypingfuriously
+python main.py
+git checkout gh-pages
+git checkout master -- _sentences
+git add _sentences
+git commit -m "Adding '_sentences' directory from 'master' branch."
+git push
