5fefe381bad740ed51ade0b6f35e45caf20df1fa
Mark Gorenstein <markagorenstein@gmail.com>
2015-05-19 12:22:58 +0100

    test

---
+++ button.js
@@ -0,0 +1,10 @@
+---
+---
+
+document.getElementsByClassName("button").onclick = function () {
+    location.href = "{{ site.baseurl }}/sentences/" + getRandomInt();
+};
+
+function getRandomInt() {
+    return Math.floor(Math.random() * {{ site.pages | size }} + 1;
+}; 
