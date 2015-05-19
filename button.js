31e769cf1a0f4247b0dec23fe0f1f7d7ed547207
Mark Gorenstein <markagorenstein@gmail.com>
2015-05-19 13:11:43 +0100

    fix js

--- button.js
+++ button.js
@@ -1,12 +1,12 @@
 ---
 ---
 
-var button = document.getElementById('button');
-button.onclick = goToNewSentence;
+var button = document.getElementById("button");
+button.onclick = goToNewSentence();
 
 function goToNewSentence() {
     location.href = "{{ site.baseurl }}/sentences/" + getRandomInt();
-}
+};
 
 function getRandomInt() {
     return Math.floor(Math.random() * {{ site.collections.sentences.docs | size }} + 1);
